cb290f07d182a8a6fbc375d89a63d82aaf680e66
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-23 20:49:00 -0500

    filter before authentication added

--- src/main/java/tech/samagua/spring_security_b010501/config/ProjectConfig.java
+++ src/main/java/tech/samagua/spring_security_b010501/config/ProjectConfig.java
@@ -1,10 +1,23 @@
 package tech.samagua.spring_security_b010501.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import tech.samagua.spring_security_b010501.filters.RequestValidatorFilter;
 
 @Configuration
 public class ProjectConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http.addFilterBefore(new RequestValidatorFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+        return http.build();
+
+    }
 
 }
